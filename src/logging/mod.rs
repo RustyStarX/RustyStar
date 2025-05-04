7fe173601d39cab35cb8de2a680dac57e6980a26
mokurin000 <1348292515a@gmail.com>
2025-05-05 05:38:47 +0800

    chore: remove elideable lifetimes

--- src/logging/mod.rs
+++ src/logging/mod.rs
@@ -2,14 +2,14 @@ use std::fmt::Debug;
 
 use spdlog::{error, warn};
 
-pub fn log_error<'a, E>(e: &'a E)
+pub fn log_error<E>(e: &E)
 where
     E: Debug,
 {
     error!("{e:?}");
 }
 
-pub fn log_warn<'a, E>(e: &'a E)
+pub fn log_warn<E>(e: &E)
 where
     E: Debug,
 {
