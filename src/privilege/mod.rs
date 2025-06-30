4e4495c52370d99eb1e712e07ffbeb2ddc32b80c
mokurin000 <1348292515a@gmail.com>
2025-07-01 00:25:56 +0800

    fix: close token handle after used

--- src/privilege/mod.rs
+++ src/privilege/mod.rs
@@ -1,7 +1,6 @@
-use spdlog::warn;
 use win32_ecoqos::windows_result;
 use windows::Win32::{
-    Foundation::{HANDLE, LUID},
+    Foundation::{CloseHandle, HANDLE, LUID},
     Security::{
         AdjustTokenPrivileges, GetTokenInformation, LUID_AND_ATTRIBUTES, LookupPrivilegeValueW,
         SE_DEBUG_NAME, SE_PRIVILEGE_ENABLED, TOKEN_ADJUST_PRIVILEGES, TOKEN_ELEVATION,
@@ -10,45 +9,42 @@ use windows::Win32::{
     System::Threading::{GetCurrentProcess, OpenProcessToken},
 };
 
-pub fn try_enable_se_debug_privilege() -> windows_result::Result<bool> {
-    let is_admin;
-    unsafe {
-        let processhandle = GetCurrentProcess();
-        let mut tokenhandle = HANDLE(std::ptr::null_mut());
-        let desiredaccess = TOKEN_ADJUST_PRIVILEGES | TOKEN_QUERY;
-        OpenProcessToken(processhandle, desiredaccess, &mut tokenhandle as _)?;
+fn is_elevated(tokenhandle: HANDLE) -> windows_result::Result<bool> {
+    let mut tokeninformation = TOKEN_ELEVATION::default();
+    let mut needed = 0_u32;
 
-        let mut tokeninformation = TOKEN_ELEVATION::default();
-        let mut needed = 0_u32;
+    unsafe {
         GetTokenInformation(
             tokenhandle,
             TokenElevation,
             Some(&mut tokeninformation as *mut _ as _),
             size_of::<TOKEN_ELEVATION>() as u32,
             &mut needed as _,
-        )?;
+        )
+        .map(|_| tokeninformation.TokenIsElevated != 0)
+    }
+}
 
-        is_admin = tokeninformation.TokenIsElevated != 0;
-
-        if tokenhandle.is_invalid() {
-            warn!("failed to open access token!");
-        }
-
-        let mut luid = LUID::default();
-        let lpname = SE_DEBUG_NAME;
+fn enable_se_debug(tokenhandle: HANDLE) -> windows_result::Result<()> {
+    let mut luid = LUID::default();
+    let lpname = SE_DEBUG_NAME;
+    unsafe {
         LookupPrivilegeValueW(None, Some(&lpname), &mut luid)?;
+    }
 
-        let attributes = SE_PRIVILEGE_ENABLED;
-        let privilege = LUID_AND_ATTRIBUTES {
-            Luid: luid,
-            Attributes: attributes,
-        };
+    let attributes = SE_PRIVILEGE_ENABLED;
+    let privilege = LUID_AND_ATTRIBUTES {
+        Luid: luid,
+        Attributes: attributes,
+    };
 
-        let disableallprivileges = false;
-        let newstate = TOKEN_PRIVILEGES {
-            PrivilegeCount: 1,
-            Privileges: [privilege],
-        };
+    let disableallprivileges = false;
+    let newstate = TOKEN_PRIVILEGES {
+        PrivilegeCount: 1,
+        Privileges: [privilege],
+    };
+
+    unsafe {
         AdjustTokenPrivileges(
             tokenhandle,
             disableallprivileges,
@@ -56,7 +52,27 @@ pub fn try_enable_se_debug_privilege() -> windows_result::Result<bool> {
             0,
             None,
             None,
-        )?;
+        )
+    }
+}
+
+pub fn try_enable_se_debug_privilege() -> windows_result::Result<bool> {
+    let is_admin;
+
+    unsafe {
+        let processhandle = GetCurrentProcess();
+        let mut tokenhandle = HANDLE(std::ptr::null_mut());
+        let desiredaccess = TOKEN_ADJUST_PRIVILEGES | TOKEN_QUERY;
+
+        OpenProcessToken(processhandle, desiredaccess, &mut tokenhandle as _)?;
+
+        is_admin = is_elevated(tokenhandle).inspect_err(|_| {
+            _ = CloseHandle(tokenhandle);
+        })?;
+
+        let enable = enable_se_debug(tokenhandle);
+        _ = CloseHandle(tokenhandle);
+        enable?;
     }
 
     Ok(is_admin)
